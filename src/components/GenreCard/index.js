e7b4f6d5523123bb1f9628a77941a074c11404dc
zoranaism <lazareviczorana@gmail.com>
2020-04-14 21:58:55 +0200

    Genres page dragging board: hardcoded multuple elements drag & drop functionality

--- src/components/GenreCard/index.js
+++ src/components/GenreCard/index.js
@@ -1,73 +1,31 @@
 import React from "react";
-// import { Button } from "react-bootstrap";
-// import Card from "react-bootstrap/Card";
-import { makeStyles } from "@material-ui/core/styles";
-import clsx from "clsx";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import Collapse from "@material-ui/core/Collapse";
-import IconButton from "@material-ui/core/IconButton";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 245,
-    position: "absolute",
-  },
-  padding: {
-    padding: "0"
-  }, 
-  expand: {
-    transform: "rotate(0deg)",
-    marginLeft: "auto",
-    padding: "0",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: "rotate(180deg)",
-  },
-}));
 
 export default function GenreCard(props) {
-  const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
+  const startDraggingHandler = (e) => {
+    props.startDragging(props.id);
   };
 
-  return (
-    <div >Genre name very long</div>
-    // <Card className={classes.root}>
-    //   <CardActions disableSpacing className={classes.padding}>
-    //   <Typography variant="overline" display="block" gutterBottom>
-    //         Genre name very long
-    //       </Typography>
-    //     <IconButton
-    //       className={clsx(classes.expand, {
-    //         [classes.expandOpen]: expanded,
-    //       })}
-    //       onClick={handleExpandClick}
-    //       aria-expanded={expanded}
-    //       aria-label="show more"
-    //     >
-    //       <ExpandMoreIcon />
-    //     </IconButton>
-    //   </CardActions>
+  const tileId = props.id === 1 ? props.value : null;
+  const tileId1 = props.id === 2 ? props.value1 : null;
+  const tileId2 = props.id === 3 ? props.value2 : null;
 
-    //   <Collapse in={expanded} timeout="auto" unmountOnExit>
-    //     <CardActions disableSpacing>
-    //       <IconButton aria-label="add to favorites" className={classes.padding}>
-    //         <FavoriteIcon />
-    //       </IconButton>
-    //       <Button size="small">Learn More</Button>
-    //     </CardActions>
-    //   </Collapse>
-    // </Card>
+  return (
+    <div
+      id={props.id}
+      style={{
+        position: "absolute",
+        width: 160,
+        height: 30,
+        border: "1px solid black",
+        ...tileId,
+        ...tileId1, 
+        ...tileId2
+      }}
+        onMouseDown={() => startDraggingHandler(props.id)}
+    >
+      {props.name}
+      <a href="#">x</a>
+    </div>
   );
 }
