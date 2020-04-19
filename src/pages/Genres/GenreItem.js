d66981035fd7afab2e38bb662545b0f645969468
zoranaism <lazareviczorana@gmail.com>
2020-04-19 16:00:13 +0200

    SVG line connected with BE, nodes styles, added dropdown when click on the node arrow

--- src/pages/Genres/GenreItem.js
+++ src/pages/Genres/GenreItem.js
@@ -1,25 +1,107 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import IconButton from "@material-ui/core/IconButton";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minWidth: 160,
+    position: "absolute",
+    paddingLeft: "5px",
+    userSelect: "none",
+  },
+  padding: {
+    padding: "0",
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    padding: "0",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+}));
 
 export default function GenreItem(props) {
-  // console.log("props", props);
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   const onMouseDownStartDragging = (event, id) => {
-    // call the callback prop passed down from the scoreboard
     props.startDragging(event, id);
   };
 
   return (
-    <div
+    <motion.div
+      whileHover={{
+        scale: 1.1,
+        transition: { duration: 0.1 },
+        cursor: "grabbing",
+      }}
+      whileTap={{
+        transition: { duration: 0.2 },
+        cursor: "grabbing",
+      }}
       style={{
         position: "absolute",
-        width: 160,
+
         height: 30,
-        border: "1px solid black",
+        textAlign: "center",
+        backgroundColor: "rgb(255, 255, 255)",
+        lineHeight: "30px",
         ...props.tile,
       }}
       onMouseDown={(event) => onMouseDownStartDragging(event, props.id)}
     >
-      {props.tile.name}
-    </div>
+      <Card className={classes.root}>
+        <CardActions disableSpacing className={classes.padding}>
+          <Typography variant="overline" display="block">
+            {props.tile.name}
+          </Typography>
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardActions disableSpacing>
+            <IconButton
+              aria-label="add to favorites"
+              className={classes.padding}
+              style={{ color: "red", height: "28px", width: "28px" }}
+            >
+              <FavoriteIcon style={{fontSize: "15px"}}/>
+            </IconButton>
+            <Button size="small">
+              <Typography variant="caption">
+                Learn More
+              </Typography>
+            </Button>
+          </CardActions>
+        </Collapse>
+      </Card>
+    </motion.div>
   );
 }
--- src/store/genres/reducer.js
+++ src/store/genres/reducer.js
@@ -1,14 +1,14 @@
 import { FETCH_GENRES_SUCCESS } from "./actions";
 // import { CREATE_ARTWORK_SUCCESS } from "./actions";
 
-const initialState = {};
+const initialState = {
+  genres: null, 
+  relations: null
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case FETCH_GENRES_SUCCESS:
-      // allGenreRelations,  response.data.allGenres
-      console.log(action.payload.allGenres);
-
       return {
         genres: [...action.payload.allGenres],
         relations: [...action.payload.allGenreRelations],
