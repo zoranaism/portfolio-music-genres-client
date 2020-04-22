0e75e2b055942fffa477eca7715f5239b54c31d6
zoranaism <lazareviczorana@gmail.com>
2020-04-22 17:16:54 +0200

    added is admin in a signup form and redux

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -26,13 +26,14 @@ const tokenStillValid = userWithoutToken => ({
 
 export const logOut = () => ({ type: LOG_OUT });
 
-export const signUp = (name, email, password) => {
+export const signUp = (name, email, isAdmin, password) => {
   return async (dispatch, getState) => {
     dispatch(appLoading());
     try {
       const response = await axios.post(`${apiUrl}/signup`, {
         name,
         email,
+        isAdmin,
         password
       });
 
